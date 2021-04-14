081a9ee6c9ca21cd5ca33a5d503e4a55af95b71b
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-04-13 21:56:34 -0700

    create review service

---
+++ services/review-service.js
@@ -0,0 +1,13 @@
+const reviewModel = require("../database/review/review-model")
+
+const findAllReviews = () => {
+    return reviewModel.find().exec()
+}
+
+const findReviewsByRecipeId = (recipeId) => {
+    return reviewModel.findById(recipeId).exec()
+}
+
+module.exports = {
+    findAllReviews, findReviewsByRecipeId
+}
