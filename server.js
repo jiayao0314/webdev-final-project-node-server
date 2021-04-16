294520c5afb337ff4ce86a95a43e3428c308e798
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-04-15 19:42:45 -0700

    add review-controller

--- server.js
+++ server.js
@@ -19,17 +19,18 @@ app.use(session({
 
 // configure CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
 require("./controllers/user-controller")(app)
 require("./controllers/profile-controller")(app)
-// require("./controllers/review-controller")(app)
+require("./controllers/review-controller")(app)
 
 app.listen(3000)
 console.log("node server is running!")
