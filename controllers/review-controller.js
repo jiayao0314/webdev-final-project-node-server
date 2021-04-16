886e9e81c47e7f0bdb4bdbeff22f32737bb138d1
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-04-15 19:44:35 -0700

    add createReviewForRecipe

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -9,15 +9,23 @@ module.exports = (app) => {
             })
     }
 
-    const findReviewsByRecipeId = (req, res) => {
+    const findReviewsByRecipe = (req, res) => {
         const recipeId = req.params['recipeId']
-        reviewService.findReviewsByRecipeId(recipeId)
+        reviewService.findReviewsByRecipe(recipeId)
+            .then((reviews) => {
+                res.send(reviews)
+            })
+    }
+
+    const createReviewForRecipe = (req, res) => {
+        reviewService.createReviewForRecipe(req.params.recipeId, req.body)
             .then((reviews) => {
                 res.send(reviews)
             })
     }
 
     app.get("/api/reviews", findAllReviews)
-    app.get("/api/reviews/:recipeId", findReviewsByRecipeId)
+    app.get("/api/reviews/:recipeId", findReviewsByRecipe)
+    app.post("/api/reviews/:recipeId", createReviewForRecipe)
 }
 
