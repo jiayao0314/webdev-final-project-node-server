23e8375e2419e1de1ba2d98e671f92e3414b8548
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-04-22 22:48:34 -0700

    add username to review

--- database/review/review-schema.js
+++ database/review/review-schema.js
@@ -4,7 +4,14 @@ const Schema = require("mongoose");
 const reviewSchema = new mongoose.Schema({
     recipeId: String,
     text: String,
-    userId: {type: Schema.Types.ObjectId, ref: 'UserModel'},
+    // userId: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'UserModel'
+    // },
+    username: {
+        type: mongoose.Schema.Types.String,
+        ref: "UserModel"
+    },
 }, {collection: "reviews", timestamps: true})
 
 module.exports = reviewSchema
--- services/review-service.js
+++ services/review-service.js
@@ -9,9 +9,9 @@ const findReviewsByRecipe = (recipeId) => {
     return reviewModel.find({recipeId: recipeId}).populate("users").exec()
 }
 
-const createReviewForRecipe = (recipeId, review, userId) => {
+const createReviewForRecipe = (recipeId, review, username) => {
     // return reviewModel.create({recipeId: recipeId, text: review.text, userId: review.user})
-    return reviewModel.create({recipeId, text: review.review.text, userId})
+    return reviewModel.create({recipeId, text: review.review.text, username})
 }
 
 const findReviewById = (reviewId) => {
