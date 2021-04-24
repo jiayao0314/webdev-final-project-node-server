786f147ca76469dabf1a230b99c2f542e6dcc465
jiayao0314 <li.jiayao@northeastern.edu>
2021-04-24 11:44:16 -0700

    add cors

--- server.js
+++ server.js
@@ -26,9 +26,12 @@ app.use(session({
     saveUninitialized: true,
 }))
 
+const cors = require('cors');
+app.use(cors({credentials: true, origin: 'http://localhost:3001'}));
+
     // `${process.env.ALLOW_ORIGIN}`
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
