644e439c29ce37f882e09bea56d0a3928630cb32
jiayao0314 <li.jiayao@northeastern.edu>
2021-04-23 22:25:38 -0700

    modify server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(session({
 
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
